db3d3fe283826a4b0b0fe4b9e7e62e8a061b50b5
Susie <susie.sko@gmail.com>
2020-10-27 07:48:38 -0700

    after lecture 31

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a react App</h1>
+        <h1>Hi, I'm a Ract App</h1>
+        <p>This is really working!</p>
       </div>
     );
     //return React.createElement('div', { className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
