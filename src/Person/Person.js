c1f2a1de4765a4094f1c8bc0035bb8729d219145
Susie <susie.sko@gmail.com>
2020-11-12 19:38:24 -0800

    after lesson 35

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+const randomNumber = () => {
+    return Math.floor(Math.random() * 30);
+}
+
 const person = () => {
-    return <p>I'm a Person!</p>;
+    return <p>I'm a Person and I am {randomNumber()} years old!</p>;
 };
 
 export default person;
