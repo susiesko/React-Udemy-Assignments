51a7fd2eb7037421fc2dc95a199a32697830011a
Susie <susie.sko@gmail.com>
2020-11-12 19:45:43 -0800

    after lecture 37

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My Hobbies: Racing</Person>
+        <Person name="Manu" age="29">
+          <p>My Hobbies: Racing</p>
+          <p>I am cool</p>
+        </Person>
         <Person name="Stephanie" age="26"/>
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,12 @@ const randomNumber = () => {
 };
 
 const person = (props) => {
-    return <p>I'm {props.name} and I am {props.age} years old!</p>;
+    return (
+        <div>
+            <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
+    );
 };
 
 export default person;
