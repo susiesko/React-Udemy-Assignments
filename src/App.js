9c645fe7289c9513c9c3486299e4afb526429055
Susie <susie.sko@gmail.com>
2020-11-12 19:42:32 -0800

    after lecture 36

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person/>
-        <Person/>
-        <Person/>
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="29">My Hobbies: Racing</Person>
+        <Person name="Stephanie" age="26"/>
       </div>
     );
     //return React.createElement('div', { className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const randomNumber = () => {
     return Math.floor(Math.random() * 30);
-}
+};
 
-const person = () => {
-    return <p>I'm a Person and I am {randomNumber()} years old!</p>;
+const person = (props) => {
+    return <p>I'm {props.name} and I am {props.age} years old!</p>;
 };
 
 export default person;
