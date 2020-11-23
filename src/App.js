a9979f531ecd79f1c9b85a60ef40f50182e78c25
susiesko <susie.sko@gmail.com>
2020-11-23 13:47:36 -0800

    after lecture 56

--- src/App.js
+++ src/App.js
@@ -49,23 +49,15 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person 
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-            changed={this.nameChangeHandler}/>
-          <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Max!')}
-            changed={this.nameChangeHandler}
-            >
-            My Hobbies: Racing<br/>
-            I am cool        
-          </Person>
-          <Person 
-            name={this.state.persons[2].name} 
-            age={this.state.persons[2].age}
-            changed={this.nameChangeHandler} />
+          {this.state.persons.map(person => {
+            return (
+              <Person
+                name={person.name}
+                age={person.age}
+                changed={this.nameChangeHandler}>                
+              </Person>              
+            );
+          })}
         </div>        
       )
     }
