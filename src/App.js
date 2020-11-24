515c426145005f90c7a536e2716d5cb6947b0e4a
susiesko <susie.sko@gmail.com>
2020-11-24 14:54:52 -0800

    after lecture 69

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -96,6 +96,7 @@ class App extends Component {
     }
     
     return (
+      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
@@ -104,6 +105,7 @@ class App extends Component {
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         { persons }
       </div>
+      </StyleRoot>
     );
   }
 }
