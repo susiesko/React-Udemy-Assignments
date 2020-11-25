3853d92ae1040c8aa9b6dcfde31f8227dd5b3488
susiesko <susie.sko@gmail.com>
2020-11-25 14:40:19 -0800

    after lecture 81

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.module.css'; // use '.module.css' extension in newer versions of react for CSS modules
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
 class App extends Component {
   state = {
@@ -55,12 +56,13 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <Person
-                key={person.id}
-                name={person.name}
-                age={person.age}
-                click={() => this.deletePersonHandler(index)}
-                changed={(event) => this.nameChangeHandler(event, person.id)}/>
+              <ErrorBoundary key={person.id}>
+                <Person                  
+                  name={person.name}
+                  age={person.age}
+                  click={() => this.deletePersonHandler(index)}
+                  changed={(event) => this.nameChangeHandler(event, person.id)}/>
+              </ErrorBoundary>
             );
           })}
         </div>        
