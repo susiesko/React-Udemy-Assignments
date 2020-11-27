e773eb9339e770e71d6555bead68b1fcfaf633fc
susiesko <susie.sko@gmail.com>
2020-11-27 15:26:12 -0800

    after lecture 98

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -35,10 +35,10 @@ const Cockpit = ( props ) => {
     btnClasses = classes.Red;
   }
 
-  if (props.persons.length <= 2){
+  if (props.personsLength <= 2){
     assignedClasses.push(classes.red); // classes = ['red']
   }
-  if (props.persons.length <= 1){
+  if (props.personsLength <= 1){
     assignedClasses.push(classes.bold); // classes = ['red', 'bold']
   }
   
@@ -51,4 +51,4 @@ const Cockpit = ( props ) => {
   );
 };
 
-export default Cockpit;
+export default React.memo(Cockpit);
