e556b8a4fb3ec85ef5d6e35ae05b402d09c7b70b
susiesko <susie.sko@gmail.com>
2020-11-27 14:05:22 -0800

    after lecture 91

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import classes from './Person.module.css';
 
-const person = props => {
-    console.log('[Persons.js] rendering...');
+class Person extends Component {
+  state = {
+
+  };
+
+  constructor(props){
+    super(props);
+    console.log('[Person.js] constructor');
+  }
+
+  // componentWillReceiveProps(props){
+  //   console.log('[Person.js] componentWillReceiveProps');    
+  // }
+
+  static getDerivedStateFromProps(props, state){
+    console.log('[Person.js] getDerivedStateFromProps');
+    return state;
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[Person.js] shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log('[Person.js] getSnapshotBeforeUpdate');
+    return {message: 'Snapshot!'};
+  }
+
+  // componentWillUpdate() {
+  //   console.log('[Person.js] componentWillUpdate');    
+  // }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[Person.js] componentDidUpdate');
+    console.log(snapshot);
+  }
+
+  render() {
+    console.log('[Person.js] rendering...');
+
     return (
         <div className={classes.Person}>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
+            <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+            <p>{this.props.children}</p>
+            <input type="text" onChange={this.props.changed} value={this.props.name}/>
         </div>
     );
+  }
+}
+
+Person.propTypes = {
+
 };
 
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,16 +1,69 @@
-import React from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import Person from './Person/Person'
 
-const persons = (props) => 
-    props.persons.map((person, index) => {
-        return (
-          <Person 
-            key={person.id}                 
-            name={person.name}
-            age={person.age}
-            click={() => props.clicked(index)}
-            changed={(event) => props.changed(event, person.id)}/>
-        );
-    });
+class Persons extends Component {
+  state = {
 
-export default persons;
+  };
+
+  constructor(props){
+    super(props);
+    console.log('[Persons.js] constructor');
+  }
+
+  // componentWillReceiveProps(props){
+  //   console.log('[Persons.js] componentWillReceiveProps');    
+  // }
+
+  static getDerivedStateFromProps(props, state){
+    console.log('[Persons.js] getDerivedStateFromProps');
+    return state;
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[Persons.js] shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return null;
+  }
+
+  // componentWillUpdate() {
+  //   console.log('[Persons.js] componentWillUpdate');    
+  // }
+
+  componentDidUpdate() {
+    console.log('[Persons.js] componentDidUpdate');    
+  }
+
+  render() {
+    console.log('[Persons.js] rendering...');
+
+    return (
+      <div>
+        {
+          this.props.persons.map((person, index) => {
+            return (
+              <Person 
+                key={person.id}                 
+                name={person.name}
+                age={person.age}
+                click={() => this.props.clicked(index)}
+                changed={(event) => this.props.changed(event, person.id)}/>
+            );
+          })
+        }     
+      </div>
+    );
+  }
+}
+
+Persons.propTypes = {
+
+};
+
+export default Persons;
