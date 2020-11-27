449d56d637e046db230bac314ff78a21f2df9a6f
susiesko <susie.sko@gmail.com>
2020-11-27 12:00:32 -0800

    after lecture 88

--- src/containers/App.js
+++ src/containers/App.js
@@ -61,7 +61,8 @@ class App extends Component {
     
     return (
       <div className={classes.Cockpit}>
-        <Cockpit 
+        <Cockpit
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}/>
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App appTitle="Persons Manager" />
   </React.StrictMode>,
   document.getElementById('root')
 );
