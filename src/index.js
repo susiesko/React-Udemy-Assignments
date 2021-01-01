b6c603bad9009411e88ba3ece2e6da44772bfd64
susiesko <susie.sko@gmail.com>
2021-01-01 12:37:39 -0800

    after electure 174

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import axios from 'axios';
+import axios from './axios';
 
 axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
 axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
