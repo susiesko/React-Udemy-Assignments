f99544ea5c211c13f0c8c5555dd972e48f0b627f
susiesko <susie.sko@gmail.com>
2021-01-01 11:52:08 -0800

    after lecture 168

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -12,34 +12,41 @@ class FullPost extends Component {
     if (this.props.id){      
       if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id != this.props.id)){
         axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
-        .then(response => {
-          this.setState({ loadedPost: response.data });
-        });
+          .then(response => {
+            this.setState({ loadedPost: response.data });
+          });
       }
     }
   }
 
+  deletePostHandler = () => {
+    axios.delete(`https://jsonplaceholder.typicode.com/posts/${this.state.loadedPost.id}`)
+      .then(response => {
+        console.log(response);
+      });
+  }
+
   render () {
-      let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+    let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
 
-      if (this.props.id) {
-        post = <p style={{textAlign: 'center'}}>Loading...!</p>;
-      }
+    if (this.props.id) {
+      post = <p style={{textAlign: 'center'}}>Loading...!</p>;
+    }
 
-      if (this.state.loadedPost){
-        post = (
-            <div className="FullPost">
-                <h1>{this.state.loadedPost.title}</h1>
-                <p>{this.state.loadedPost.body}</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+    if (this.state.loadedPost){
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.body}</p>
+          <div className="Edit">
+            <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
+          </div>
+        </div>
 
-        );
-      }     
+      );
+    }     
 
-      return post;
+    return post;
   }
 }
 
