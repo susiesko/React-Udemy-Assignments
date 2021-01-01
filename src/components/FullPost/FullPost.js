e413d6928f81346d7c7cbdde6efc37c71470835d
susiesko <susie.sko@gmail.com>
2020-12-31 18:35:02 -0800

    part way through 166

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+  render () {
+      let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
 
-        if (this.props.id){
-          post = (
-              <div className="FullPost">
-                  <h1>Title</h1>
-                  <p>Content</p>
-                  <div className="Edit">
-                      <button className="Delete">Delete</button>
-                  </div>
-              </div>
-  
-          );
-        }
-        
+      if (this.props.id){
+        post = (
+            <div className="FullPost">
+                <h1>Title</h1>
+                <p>Content</p>
+                <div className="Edit">
+                    <button className="Delete">Delete</button>
+                </div>
+            </div>
 
-        return post;
-    }
+        );
+      }
+      
+
+      return post;
+  }
 }
 
 export default FullPost;
