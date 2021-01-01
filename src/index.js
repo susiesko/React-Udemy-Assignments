505e37d98016b1d09d3352c947d63362c6ffe0a8
susiesko <susie.sko@gmail.com>
2021-01-01 12:11:15 -0800

    after lecture 173

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json'; // this is the default value but we are including it for demonstration's sake
+
 axios.interceptors.request.use(request => {
   console.log(request);
   // Edit request config
