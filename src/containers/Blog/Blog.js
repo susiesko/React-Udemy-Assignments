29d126d49a947245ad969e34c798dd067f5b6fb0
susiesko <susie.sko@gmail.com>
2020-12-31 18:22:02 -0800

    after lecture 165

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [], 
+    selectedPostId: null
   };
 
   componentDidMount() {
@@ -24,12 +25,17 @@ class Blog extends Component {
     );
   }
 
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  }
+
   render () {
     const posts = this.state.posts.map(post => {
       return <Post 
         key={post.id} 
         title={post.title} 
-        author={post.author} />;
+        author={post.author} 
+        clicked={() => this.postSelectedHandler(post.id)} />;
     });
 
     return (
@@ -38,7 +44,7 @@ class Blog extends Component {
           { posts }
         </section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
