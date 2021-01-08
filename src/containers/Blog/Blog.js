ed41aa6d912aee2d82e72a9eda08c39172a11fca
Susie <susie.sko@gmail.com>
2021-01-07 19:06:09 -0800

    after lecture 201

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import Post from '../../components/Post/Post';
@@ -14,14 +14,22 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><Link to='/'>Home</Link></li>
-              <li><Link 
+              <li><NavLink 
+                to='/' 
+                exact
+                activeClassName="my-active"
+                activeStyle={{
+                  color: '#af00bd',
+                  textDecoration: 'underline'
+                }}>Home
+              </NavLink></li>
+              <li><NavLink 
                 to={{
                   pathname: '/new-post',
                   hash: '#submit',
                   search: '?quick-submit=true'
                 }}>New Post
-              </Link></li>
+              </NavLink></li>
             </ul>
           </nav>
         </header>
