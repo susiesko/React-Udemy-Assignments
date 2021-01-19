a62ffafd3d279c6362539c226f9cb546bc0b65aa
Susie <susie.sko@gmail.com>
2021-01-18 19:29:04 -0800

    after lecture 257

--- public/redux-basics.js
+++ public/redux-basics.js
@@ -25,11 +25,13 @@ const rootReducer = (state = initialState, action) => {
 
 // Store
 const store = createStore(rootReducer);
-console.log(store.getState());
+console.log('initial state', store.getState());
+
+// Subscription
+store.subscribe(() => {
+  console.log('[Subscription]', store.getState());
+});
 
 // Dispatching Action
 store.dispatch({ type: 'INC_COUNTER' });
 store.dispatch({ type: 'ADD_COUNTER', value: 10});
-console.log(store.getState());
-
-// Subscription
