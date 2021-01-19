2edb8530e7628f7f4136ddd2af0da829a1d3e0ed
susiesko <susie.sko@gmail.com>
2021-01-19 14:14:00 -0800

    after lecture 267

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -38,7 +38,7 @@ class Counter extends Component {
         <CounterControl label="Add 5" clicked={this.props.onAdd} />
         <CounterControl label="Subtract 5" clicked={this.props.onSubtract} />
         <hr/>
-        <button onClick={this.props.onStoreResult}>Store Result</button>
+        <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
         <ul>
           {
             this.props.storedResults.map((storedResult) => (
@@ -58,8 +58,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
   return {
-    ctr: state.counter,
-    storedResults: state.results
+    ctr: state.ctr.counter,
+    storedResults: state.res.results
   };
 };
 
@@ -69,7 +69,7 @@ const mapDispatchToProps = dispatch => {
     onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
     onAdd: () => dispatch({type: actionTypes.ADD, val: 5}),
     onSubtract: () => dispatch({type: actionTypes.SUBTRACT, val: 5}),
-    onStoreResult: () => dispatch({type: actionTypes.STORE_RESULT}),
+    onStoreResult: (ctrResult) => dispatch({type: actionTypes.STORE_RESULT, result: ctrResult}),
     onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, idToDelete: id})
   };
 };
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,30 +1,21 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions';
 
 const initialState = {
-  counter: 0,
   results: []
 };
 
 const reducer = (state = initialState, action) => {
   switch(action.type){
-    case actionTypes.INCREMENT:
-      return { ...state, counter: state.counter + 1 };
-    case actionTypes.DECREMENT:
-      return { ...state, counter: state.counter - 1 };
-    case actionTypes.ADD:
-      return { ...state, counter: state.counter + action.val };
-    case actionTypes.SUBTRACT:
-      return { ...state, counter: state.counter - action.val };
     case actionTypes.STORE_RESULT: {
       // I personally do not find 'new Date()' to be a very good identifier given the possibility of duplicate keys,
       // but I am just following along with the course. 
       // Update in lecture 264: I'm going to use Date.now() since a bit more precise
       return { 
         ...state, 
-        results: state.results.concat({ id: Date.now(), value: state.counter }) 
+        results: state.results.concat({ id: Date.now(), value: action.result }) 
       };
     }
-    case actionTypes.DELETE_RESULT: {
+    case actionTypes.DELETE_RESULT: {      
       let results = state.results.filter(cur => cur.id !== action.idToDelete);
 
       return { ...state, results };
