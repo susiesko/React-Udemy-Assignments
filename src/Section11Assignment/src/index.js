9417d5b83a4ced9bea1d2df1667dd3dc3601be88
Susie <susie.sko@gmail.com>
2021-01-18 18:59:42 -0800

    moving section 11 files

--- src/Section11Assignment/src/index.js
+++ src/Section11Assignment/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from '../../registerServiceWorker';
 import axios from './axios';
 
 axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
