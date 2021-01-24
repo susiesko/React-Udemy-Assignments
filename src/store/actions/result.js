71950ac4590d715e9a9ff9fa3f51d34889b9f1cf
Susie <susie.sko@gmail.com>
2021-01-23 19:12:57 -0800

    after lecture 291

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -1,6 +1,6 @@
 import * as actionTypes from './actionTypes';
 
-export const saveResult = res => {
+export const saveResult = res => {  
   return {
     type: actionTypes.STORE_RESULT,
     ...res
